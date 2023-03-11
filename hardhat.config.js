cb7e6944f9fcb2a1daa7517ed9ad8bced04bdcfd
fizasahar12 <fizas7126@gmail.com>
2023-03-11 20:39:41 +0500

    hello

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
+const hre = require("hardhat");
+
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {
+    },
+    goerli: {
+      url: "https://goerli.infura.io/v3/",
+      accounts: [c68ab45dc9f95dc3574c50d138642a28ef7249e0966196ed814d67df0b2c9af6
+      ]
+    }
+  },
+
   solidity: "0.8.4",
   paths: {
     artifacts: "./src/backend/artifacts",
